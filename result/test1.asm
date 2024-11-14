4f079a91879d1fccebd0225b09a7606012106f0c
chi <rk24876@bristol.ac.uk>
2024-11-14 16:57:40 +0000

    simulate test1.asm code

--- result/test1.asm
+++ result/test1.asm
@@ -3,8 +3,21 @@ D=A
 @SP
 M=D
 // push constant
+@7
+D=A
+@SP
+A=M
+M=D // RAM[256]=7
+@SP
+A=A+1
 // push constant
+@8
+D=A
+@SP
+A=M
+M=D // RAM[257]=8
 // add
+// RAM[256] = 15 (?)
 (HaltInfiniteLoop)
 @HaltInfiniteLoop
 0;JMP
